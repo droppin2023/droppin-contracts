59e9091b67ef9a1158fd5080ce8a3f9fe4ca158d
CarlosR <carlos.ramos@grupoimportia.com>
2023-01-13 18:49:13 +0800

    feat : linked condition to zkp request

--- contracts/Droppin.sol
+++ contracts/Droppin.sol
@@ -2,34 +2,51 @@ pragma solidity ^0.8.0;
 
 import "./Storage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./verifiers/ZKPVerifier.sol";
+import "./interfaces/ICircuitValidator.sol";
 /**
 **  @dev Droppin Protocol
 **  @author Carlos Ramos
 **  @note Most of the mapping data can be stored off chain for better performance
 */
-contract Droppin {
+contract Droppin is ZKPVerifier{
     using Counters for Counters.Counter;
 
     Counters.Counter internal groupIds;
     Counters.Counter internal conditionIds;
+    Counters.Counter internal verifierRequestIds;
 
-    mapping(uint256 => GroupData) groupById;
-    mapping(address => Counters.Counter) groupByUserCounter;
-    mapping(uint256 => ConditionData) conditionsById;
+    address internal validatorAddr;
+    string internal circuitId;
 
-    constructor () {
-        groupIds.increment();
+    mapping(uint256 => GroupData) public groupById;
+    mapping(address => Counters.Counter) public groupByUserCounter;
+    mapping(uint256 => ConditionData) public conditionsById;
+
+    constructor(address _validatorAddr, string memory _circuitId) {
+        validatorAddr = _validatorAddr;
+        circuitId = _circuitId;
     }
 
     function createGroup(GroupData memory _newGroup) external {
+        groupIds.increment();
         groupById[groupIds.current()] = _newGroup;
-        groupByUserCounter[msg.sender].increment();
+        groupByUserCounter[_newGroup.owner].increment();
     }
 
-    function addCondition(uint256 _groupId, ConditionData memory _newCondition) external {
+
+    function addCondition(uint256 _groupId, uint256 _schemaHash) external {
         _IsGroupOwner(msg.sender,_groupId);
         conditionIds.increment();
-        conditionsById[conditionIds.current()] = ConditionData(_groupId,_newCondition.schemaHash);
+        verifierRequestIds.increment();
+        conditionsById[conditionIds.current()] = ConditionData(_groupId,_schemaHash);
+        // query is fixed to 1 attribute which is boolean and also is equal to 1
+        uint256[] memory value = new uint256[](64);
+        value[0] = 1;
+
+        //TODO : fix parsing issue from bigger int to small one, might cause problem in the future
+        setZKPRequest(uint64(verifierRequestIds.current()), ICircuitValidator(validatorAddr), ICircuitValidator.CircuitQuery(_schemaHash,2,1,value,circuitId));
+        
     }
 
     function _IsGroupOwner (address _user, uint256 _groupId) internal view {
