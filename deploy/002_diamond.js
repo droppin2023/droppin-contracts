d751be5da63dcd766cf4fd3783f554fcdf5b147a
CarlosR <carlos.ramos@grupoimportia.com>
2023-01-20 21:21:57 +0800

    feat : testing

--- deploy/002_diamond.js
+++ deploy/002_diamond.js
@@ -80,89 +80,36 @@ const deployAll = async () => {
   );
 
   // scripts for testing on backend
-  const cCoreFacetProxy = await ethers.getContractAt(
-    "CoreFacet",
-    dDroppin.address
-  );
-  let tx = await cCoreFacetProxy
-    .connect(sDeployer)
-    .createGroup(formatBytes32String("Lepak DAO"));
-  let receipt = await tx.wait();
-  //   console.log(receipt);
-  let groupCreatedEvent;
-  console.log("Create Group Tx : ", receipt.transactionHash);
-  for (const event of receipt.logs) {
-    try {
-      const parsedLog = cCoreFacetProxy.interface.parseLog(event);
-      //   console.log(parsedLog);
-      if (parsedLog && parsedLog.name === "GroupCreated") {
-        groupCreatedEvent = parsedLog;
-        break;
-      }
-    } catch (e) {}
-  }
 
-  // add quests
-  const questsToAdd = [
-    {
-      name: formatBytes32String("Quest1"),
-      groupId: 1,
-      engagePoints: 750,
-      owner: sDeployer,
-    },
-    {
-      name: formatBytes32String("Quest2"),
-      groupId: 1,
-      engagePoints: 2000,
-      owner: sDeployer,
-    },
-    {
-      name: formatBytes32String("Quest3"),
-      groupId: 1,
-      engagePoints: 500,
-      owner: sDeployer,
-    },
-    {
-      name: formatBytes32String("Quest4"),
-      groupId: 1,
-      engagePoints: 100,
-      owner: sDeployer,
-    },
-    {
-      name: formatBytes32String("Quest5"),
-      groupId: 1,
-      engagePoints: 150,
-      owner: sDeployer,
-    },
-  ];
+//   let tx2 = await cCoreFacetProxy
+//     .connect(sDeployer)
+//     .modifyGroup(
+//       1,
+//       formatBytes32String("Lepak DAO"),
+//       "0xB7a0D386fA245C1E72Ca26127bA25e75816769f2"
+//     );
+//     await tx2.wait();
+  //   const badgeData = {
+  //     requiredQuests: [1, 4, 0],
+  //     engagePointsThreshold: 1000,
+  //     badgePrice: parseEther("0.01"),
+  //     name: "Hacker Badge",
+  //     NFT: ethers.constants.AddressZero,
+  //     groupId: 1,
+  //     owner: sDeployer,
+  //     symbol: "HACK",
+  //     URI: "www.google.com",
+  //   };
 
-  questsToAdd.forEach(async (item,id) => {
-    const tx = await cCoreFacetProxy.connect(item.owner).addQuest(item);
-    const receipt = await tx.wait();
-    console.log("Create Quest %d : ", id+1, receipt.transactionHash);
-  });
-
-  const badgeData = {
-    requiredQuests: [1, 4, 0],
-    engagePointsThreshold: 1000,
-    badgePrice: parseEther("0.01"),
-    name: "Hacker Badge",
-    NFT: ethers.constants.AddressZero,
-    groupId: 1,
-    owner: sDeployer,
-    symbol: "HACK",
-    URI: "www.google.com",
-  };
-
-  const cBadgeFacetProxy = await ethers.getContractAt(
-    "BadgeFacet",
-    dDroppin.address
-  );
-  tx = await cBadgeFacetProxy
-    .connect(badgeData.owner)
-    .addBadge(badgeData, badgeData.symbol, badgeData.URI);
-  receipt = await tx.wait();
-  console.log("Create badge Tx : ", receipt.transactionHash);
+  //   const cBadgeFacetProxy = await ethers.getContractAt(
+  //     "BadgeFacet",
+  //     dDroppin.address
+  //   );
+  //   tx = await cBadgeFacetProxy
+  //     .connect(badgeData.owner)
+  //     .addBadge(badgeData, badgeData.symbol, badgeData.URI);
+  //   receipt = await tx.wait();
+  //   console.log("Create badge Tx : ", receipt.transactionHash);
 };
 
 const main = async () => {
@@ -170,5 +117,5 @@ const main = async () => {
 };
 
 main.id = "001_core";
-main.skip = () => false;
+main.skip = () => true;
 module.exports = main;
